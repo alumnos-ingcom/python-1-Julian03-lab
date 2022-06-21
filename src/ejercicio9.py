1e788f2d7c139add1cba75d3fe01057373127417
Julian <julian03yt@gmail.com>
2022-06-21 19:38:53 -0300

    Ejercicio 9 comenzado

---
+++ src/ejercicio9.py
@@ -0,0 +1,21 @@
+################
+# Julian Fontana - @Julian03-lab
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+Escribir una función que retorne una tuple con factores primos de un numero entero positivo.
+"""
+# Reemplazar por las funciones del ejercicio
+
+
+def principal():  # pragma: no cover
+    """
+    Esta función es la que se encarga de la parte 'interactiva' del ejercicio
+    (La entrada, la llamada al algoritmo y la salida)
+    """
+    pass
+
+if __name__ == "__main__":  # pragma: no cover
+    principal()
+
