1dab1cb4522ed704217b4ead010517941b14c599
Julian03-lab <julian03yt@gmail.com>
2022-06-22 17:22:10 -0300

    creada lista para definir los numeros primos de 1 a n

--- src/ejercicio9.py
+++ src/ejercicio9.py
@@ -6,16 +6,36 @@
 """
 Escribir una función que retorne una tuple con factores primos de un numero entero positivo.
 """
-# Reemplazar por las funciones del ejercicio
-def factores_primos()
-    pass
+def es_primo(numero:int):
+    """
+    Recibe como atributo un entero. Retorna True si el numero es primo y False si no es primo.
+    """
+    count = 2
+    cantidad_restos = 0
 
+    while count < numero:
+        resto = numero%count
+        if resto == 0:
+            cantidad_restos +=1
+        count += 1
+    return(cantidad_restos==0)
+
+
+def factores_primos(numero):
+    numeros_primos = []
+    for i in range(1,numero+1):
+        if es_primo(i):
+            numeros_primos.append(i)
+    return(numeros_primos)
+
+
+    
 def principal():  # pragma: no cover
     """
     Esta función es la que se encarga de la parte 'interactiva' del ejercicio
     (La entrada, la llamada al algoritmo y la salida)
     """
-    pass
+    print(factores_primos(40))
 
 if __name__ == "__main__":  # pragma: no cover
     principal()
